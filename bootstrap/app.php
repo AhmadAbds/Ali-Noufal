5b264fd5c471bda4f9b36ffb5b4c7998ce89ba3a
eng-ahmad-alhamwi <linaahmadabd@gmail.com>
2025-12-12 19:28:38 +0300

    aaaa

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,6 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'api/*' ,
             'sanctum/csrf-cookie',
         ]);
+        $middleware->trustProxies(at: '*');
          $middleware->web([]);
         $middleware->api([]);
     })
